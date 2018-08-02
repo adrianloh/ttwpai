9b163d76a730c71e7dc6d42a50e8022749a79e7f
Adrian <adrianloh@gmail.com>
2018-08-02 22:26:58 +0800

    Initial commit

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
-ln -fs Startup /Applications/Adobe\ Illustrator\ CC\ 2018/Startup\ Scripts
-ln -fs Scripts /Applications/Adobe\ Illustrator\ CC\ 2018/Presets.localized/en_US/Scripts
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+SRC=${SCRIPTPATH}/Startup
+ln -fs $SRC /Applications/Adobe\ Illustrator\ CC\ 2018/Startup\ Scripts
+echo $SRC
+
+SRC=${SCRIPTPATH}/Scripts
+echo $SRC
+ln -fs $SRC /Applications/Adobe\ Illustrator\ CC\ 2018/Presets.localized/en_US/Scripts
